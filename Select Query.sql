1c2be4ba4d695c3a1ce45e26235d2eb2341970d1
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-14 15:32:21 +0530

    MySql Query commit v3

--- Select Query.sql
+++ Select Query.sql
@@ -1,29 +1,39 @@
+/* Create a separate select queries to get a hobby, employee, employee_salary, employee_hobby. */
+
+select * from employee_hobby
+inner join hobby
+on employee_hobby.fk_hobby = hobby.id
+inner join employee 
+on employee_hobby.fk_employee = employee.id
+inner join employee_salary
+on employee_salary.fk_employee = employee_hobby.id;
+
 /* Create a select single query to get all employee name, all hobby_name in single column */
 
 SELECT name FROM hobby
 UNION ALL
-SELECT CONCAT(First_Name,' ', Last_Name) FROM employee;
+SELECT CONCAT(first_name,' ', last_name) FROM employee;
 
 /* Create a select query to get  employee name, his/her employee_salary */
 
-select employee.First_Name, employee_salary.salary from employee 
+select employee.first_name, employee_salary.salary from employee 
 inner join employee_salary
-on employee_salary.FK_of_employee = employee.id;
+on employee_salary.fk_employee_id = employee.id;
 
 /* Create a select query to get employee name, total salary of employee, hobby name(comma-separated - you need to use subquery for hobby name) */
 
 SELECT
-employee.First_Name,
-employee.Last_Name,
-employee_salary.Salary,
+employee.first_name,
+employee.last_name,
+employee_salary.salary,
 (
 SELECT GROUP_CONCAT(distinct h.name order by h.name SEPARATOR ', ' )
 FROM hobby h
 INNER JOIN employee_hobby eh 
 ON h.id = eh.fk_hobby
-WHERE eh.fk_of_employee = employee.Id
+WHERE eh.fk_employee_id = employee.id
 ) AS Hobby_Names
 FROM
 employee
 JOIN
-employee_salary ON employee.Id = employee_salary.FK_of_employee;
+employee_salary ON employee.id = employee_salary.fk_employee_id;
